24e55c2f8ebb33c83300ddefc42c8979c7d280cb
CAGULA770 <166716920+CAGULA770@users.noreply.github.com>
2024-10-05 10:21:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=nNVUBJhK#RjX_SUEmReGs8pyxKlzxUlDCyZGR4qJE1t5-GlWCf-A",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
